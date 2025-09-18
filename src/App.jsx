822bf25394f2b9bac1a0a2b32ab9dd5965f4cee0
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-18 20:36:19 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-center font-bold text-5xl'> React</h1>
+      <Countries/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2 className='text-center font-bold text-5xl mt-8'>I am Softwate developer</h2>
+        </div>
+    );
+};
+
+export default Countries;
